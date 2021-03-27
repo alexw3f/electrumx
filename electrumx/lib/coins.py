bd928d1393d590a095c218b49b81645524039827
notafoolsgarden <37415622+notafoolsgarden@users.noreply.github.com>
2021-03-28 00:38:52 +0100

    Update coins.py
    Add Garlicoin (GRLC)

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -3919,3 +3919,22 @@ class Beyondcoin(Coin):
     TX_PER_BLOCK = 2
     RPC_PORT = 10332
     REORG_LIMIT = 5000
+
+
+class Garlicoin(Coin):
+    NAME = "Garlicoin"
+    SHORTNAME = "GRLC"
+    NET = "mainnet"
+    XPUB_VERBYTES = bytes.fromhex("0488b21e")
+    XPRV_VERBYTES = bytes.fromhex("0488ade4")
+    P2PKH_VERBYTE = bytes.fromhex("26")
+    P2SH_VERBYTES = [bytes.fromhex("32"), bytes.fromhex("05")]
+    WIF_BYTE = bytes.fromhex("b0")
+    GENESIS_HASH = ('2ada80bf415a89358d697569c96eb98c'
+                    'dbf4c3b8878ac5722c01284492e27228')
+    DESERIALIZER = lib_tx.DeserializerSegWit
+    TX_COUNT = 1031869
+    TX_COUNT_HEIGHT = 2343659
+    TX_PER_BLOCK = 2
+    RPC_PORT = 42068
+    REORG_LIMIT = 800
